f84aaf811cd79e0bf833c654ac98eb73703a29d7
chavierto <hola@xavierlujan.com>
2022-09-12 17:37:43 -0400

    sort NFTs by token id

--- src/NftGrid.js
+++ src/NftGrid.js
@@ -5,6 +5,8 @@ function NftGrid(props) {
 	// Store props nfts into variable
 	const nfts = props.nfts;
 
+	// Sort NFTs by token ID
+	nfts.sort((a,b) => a.token.token_id - b.token.token_id)
 	// Return grid with nfts variable mapped into divs with the image
 	return (
 		<div className='site-header'>
